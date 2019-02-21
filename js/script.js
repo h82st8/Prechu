d8a8bd3cab3e65a958544776c292a78117fe978d
Ilya <h82st8@gmail.com>
2019-02-21 11:15:59 +0300

    start

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+	// $(window).on("load resize", function(){
+ //    var width = $(document).width();
+    
+ //    if (width < 641) {
+ //      $('.carousel').slick('unslick');
+ //    }
+ //    else {
+ //        $('.carousel').not('.slick-initialized').slick({
+ //          infinite: true
+ //        });
+ //    }
+	// });
+	$('.carousel').carousel({
+	});
+});
